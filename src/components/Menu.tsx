fe3e6112ca863d022b9df676fba87aabd3815c89
ryann254 <keddelyronjoz@gmail.com>
2023-05-24 16:25:01 +0300

    feat: add menu items

---
+++ src/components/Menu.tsx
@@ -0,0 +1,78 @@
+import { useSpring, animated } from '@react-spring/web';
+import { SetStateAction } from 'react';
+
+export const MenuBars = ({
+  setMenu,
+}: {
+  setMenu: React.Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <div
+      className='absolute top-5 right-5 cursor-pointer'
+      onClick={() => setMenu(true)}
+    >
+      <div className='bg-[#FFFFFF] h-0.5 w-6 mb-1'></div>
+      <div className='bg-[#FFFFFF] h-0.5 w-6 mb-1'></div>
+      <div className='bg-[#FFFFFF] h-0.5 w-6'></div>
+    </div>
+  );
+};
+
+const menuItems = [
+  'Search Homes',
+  'Tour Virtually',
+  'Book a Tour',
+  'Apply Digitally',
+  'E-sign Contract',
+  'Move In Safely',
+];
+
+const MenuItems = ({ index, item }: { index: number; item: string }) => (
+  <div className='flex flex-col mx-5 mb-10'>
+    <div className='flex mb-[18px] cursor-pointer'>
+      <p className='mr-5'>0{index + 1}.</p>
+      <p className='uppercase'>{item}</p>
+    </div>
+    <div className='h-px w-full bg-[#000000]'></div>
+  </div>
+);
+
+const Menu = ({
+  show,
+  setShow,
+}: {
+  show: boolean;
+  setShow: React.Dispatch<SetStateAction<boolean>>;
+}) => {
+  const props = useSpring({
+    from: { x: 0, opacity: 0 },
+    to: { x: show ? 0 : 100, opacity: show ? 1 : 0 },
+  });
+  return (
+    <animated.div
+      style={{
+        left: show ? 0 : window.innerWidth,
+        position: 'absolute',
+        top: 0,
+        backgroundColor: '#FFFFFF',
+        height: '100vh',
+        width: '100%',
+        ...props,
+      }}
+    >
+      <div
+        className='mt-5 mr-5 float-right cursor-pointer'
+        onClick={() => setShow(false)}
+      >
+        <i className='fa-solid fa-xmark text-xl'></i>
+      </div>
+      <div className='h-full w-full flex flex-col justify-center'>
+        {menuItems.map((item, index) => (
+          <MenuItems key={index} index={index} item={item} />
+        ))}
+      </div>
+    </animated.div>
+  );
+};
+
+export default Menu;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import LandingPage from '@/public/home/mobile-landing-page.png';
+import Menu, { MenuBars } from '@/components/Menu';
+import { useState } from 'react';
+
 const Home = () => {
-  return <div>Homepage</div>;
+  const [show, setShow] = useState(false);
+  return (
+    <div className='relative w-full h-full'>
+      <img
+        src={LandingPage}
+        alt='Home'
+        className='w-full h-full object-cover'
+      />
+      <div className='shadow border-0 absolute top-0 left-0 h-12 w-full'></div>
+      <MenuBars setMenu={setShow} />
+      <Menu show={show} setShow={setShow} />
+    </div>
+  );
 };
 
 export default Home;
