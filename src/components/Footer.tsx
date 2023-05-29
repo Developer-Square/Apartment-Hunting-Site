9ddb6b58aaaf48f87379a95c33c876a5838ecfb8
ryann254 <keddelyronjoz@gmail.com>
2023-05-29 07:52:30 +0300

    feat: add back to top functionality

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,9 @@
 const Footer = () => {
+  const topFunction = () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  };
+
   return (
     <div className='mt-16 border-t-2 border-[#5D5D5D]'>
       <div className='mt-3.5 flex flex-col items-center'>
@@ -9,7 +14,10 @@ const Footer = () => {
           Terms of Service
         </p>
         <p className='text-white/[.7] text-sm cursor-pointer'>Privacy Policy</p>
-        <div className='flex items-center my-3.5 cursor-pointer'>
+        <div
+          className='flex items-center my-3.5 cursor-pointer'
+          onClick={() => topFunction()}
+        >
           <p>Back to top</p>
           <i className='fa-solid fa-arrow-up ml-2'></i>
         </div>
