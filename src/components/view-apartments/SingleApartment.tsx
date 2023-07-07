2005d3ff933da4df0a3eebe7c1da936f19a77cac
ryann254 <keddelyronjoz@gmail.com>
2023-07-07 07:21:52 +0300

    feat: done with 360px view

--- src/components/view-apartments/SingleApartment.tsx
+++ src/components/view-apartments/SingleApartment.tsx
@@ -20,35 +20,16 @@ const SingleApartment = ({ info }: { info: ApartmentInfoProps }) => {
     ViewApartments4,
     ViewApartments5,
   ];
-  const [isHovered, setIsHovered] = useState(false);
   const [isApartmentHovered, setIsApartmentHovered] = useState(false);
   const [isSaved, setIsSaved] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [swiperQualities, setswiperQualities] = useState([Pagination]);
-
-  const { x } = useSpring({
-    from: { x: 0 },
-    x: isHovered ? 1 : 0,
-    config: { duration: 1000 },
-  });
 
   const { x: apartmentX } = useSpring({
     from: { x: 0 },
     x: isApartmentHovered ? 1 : 0,
     config: { duration: 1000 },
   });
-
-  const handleHoverState = (hover: string) => {
-    if (isSaved) return;
-
-    if (hover === 'isEnter') {
-      setIsHovered(true);
-      setIsApartmentHovered(false);
-      return;
-    }
-    setIsHovered(false);
-    setIsApartmentHovered(false);
-  };
 
   useEffect(() => {
     if (window.innerWidth > 640) {
@@ -75,25 +56,6 @@ const SingleApartment = ({ info }: { info: ApartmentInfoProps }) => {
         onMouseLeave={() => setIsApartmentHovered(false)}
       >
         <div className='relative w-full h-80'>
-          <animated.div
-            style={{
-              scale: x.to({
-                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
-              }),
-            }}
-            onMouseEnter={() => handleHoverState('isEnter')}
-            onMouseLeave={() => handleHoverState('isLeave')}
-            onClick={() => setIsSaved(!isSaved)}
-          >
-            <div className='relative'>
-              {isSaved ? (
-                <i className='absolute top-2.5 cursor-pointer right-4 text-2xl fa-solid text-red-500 fa-heart'></i>
-              ) : (
-                <i className='absolute top-2.5 cursor-pointer right-4 text-2xl fa-regular fa-heart'></i>
-              )}
-            </div>
-          </animated.div>
           <Swiper
             modules={swiperQualities}
             navigation={swiperQualities.includes(Navigation) ? true : false}
@@ -104,33 +66,46 @@ const SingleApartment = ({ info }: { info: ApartmentInfoProps }) => {
           >
             {viewApartments.map((apartment, index) => (
               <SwiperSlide key={index}>
-                <img
-                  src={apartment}
-                  className='w-full h-full rounded-xl'
-                  alt='view apartments'
-                />
+                <>
+                  <div
+                    className='relative'
+                    onClick={() => setIsSaved(!isSaved)}
+                  >
+                    {isSaved ? (
+                      <i className='absolute top-2.5 cursor-pointer right-4 text-2xl fa-solid text-red-500 fa-heart'></i>
+                    ) : (
+                      <i className='absolute top-2.5 text-black cursor-pointer right-4 text-2xl fa-regular fa-heart'></i>
+                    )}
+                  </div>
+
+                  <img
+                    src={apartment}
+                    className='w-full h-full rounded-xl'
+                    alt='view apartments'
+                  />
+                  <animated.div
+                    style={{
+                      scale: apartmentX.to({
+                        range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                        output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+                      }),
+                    }}
+                    className='absolute bottom-3.5 cursor-pointer left-2.5 h-[76px] w-[70px] flex preview'
+                    onClick={() => setShowModal(true)}
+                  >
+                    <div className='h-full w-3 border-[1.8px] border-r-[#C6B1A5]/[.6]'></div>
+                    <div className='flex h-full w-full justify-center items-center'>
+                      <img
+                        src={propertyManager}
+                        alt='profile'
+                        className='rounded-full w-10 h-10'
+                      />
+                    </div>
+                  </animated.div>
+                </>
               </SwiperSlide>
             ))}
           </Swiper>
-          <animated.div
-            style={{
-              scale: apartmentX.to({
-                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
-              }),
-            }}
-            className='absolute bottom-2.5 cursor-pointer left-2.5 h-20 w-[78px] flex preview'
-            onClick={() => setShowModal(true)}
-          >
-            <div className='h-full w-3 border-[1.8px] border-r-[#C6B1A5]/[.6]'></div>
-            <div className='flex h-full w-full justify-center items-center'>
-              <img
-                src={propertyManager}
-                alt='profile'
-                className='rounded-full w-12 h-12'
-              />
-            </div>
-          </animated.div>
         </div>
         <div className='mt-3.5'>
           <p className='font-bold'>{title}</p>
