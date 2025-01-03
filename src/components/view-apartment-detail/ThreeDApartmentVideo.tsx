1cee4402a1a5a1dc48192fbbce605ff0c6e591ed
ryann254 <keddelyronjoz@gmail.com>
2025-01-03 16:44:42 +0300

    feat: add 3d apartment model

--- src/components/view-apartment-detail/ThreeDApartmentVideo.tsx
+++ src/components/view-apartment-detail/ThreeDApartmentVideo.tsx
@@ -1,28 +1,14 @@
-import ApartmentVideo from '@/assets/view-apartment-detail-page/video_3.mp4';
-import ApartmentVideo2 from '@/assets/view-apartment-detail-page/video_1.mp4';
-import { useEffect, useState } from 'react';
 
 const ThreeDApartmentVideo = () => {
-  const [videoSrc, setVideoSrc] = useState(ApartmentVideo);
-
-  useEffect(() => {
-    if (window.innerWidth >= 640) {
-      setVideoSrc(ApartmentVideo2);
-      return;
-    }
-    setVideoSrc(ApartmentVideo);
-  }, []);
-
   return (
-    <div className='relative h-[630px] w-full mx-6' id='video-tour'>
-      <video
-        width='100%'
-        height='100%'
-        className='w-full h-full absolute top-0 left-[50%] transform translate-x-[-50%]'
-        controls={true}
-      >
-        <source src={videoSrc} type='video/mp4' />
-      </video>
+    <div className="relative h-[600px] w-full mt-6">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://my.matterport.com/show/?m=UrZuxFAt6j6"
+        allowFullScreen
+        allow="autoplay; fullscreen; web-share; xr-spatial-tracking;"
+      ></iframe>
     </div>
   );
 };
